4d0e5421b823eeb7f696fe31f960310a3531e989
Pavel Kruglik <pavel.k@symfonyart.com>
2023-02-25 17:08:50 +0300

    feat: added few more TODO components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,12 @@
 // The list of potential components:
 // 1. Navigation
 // 2. DayCard
-// ...
+// 3. Temperature
+// 4. Temperature container (growing margin due to temperature growing) 
+// 5. Wind speed
+// 6. Search Autocomplete
+// 7. Base card container
+// 8. Current weather: 
+      // - sun path
 
 export {};
