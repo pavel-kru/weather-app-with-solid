deec0a369d96fa984ff323e87530c1c34d056db7
Pavel Kruglik <pavel.k@symfonyart.com>
2023-02-26 10:34:03 +0300

    feat: added env files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import type { Component } from "solid-js";
+import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <div class="container mx-auto p-4 min-h-screen bg-main-bg">
-      <header class="text-2xl font-semibold">Gismeteo Clone</header>
-      <main class="container mx-auto py-4">Content</main>
+    <div class='container mx-auto p-4 min-h-screen bg-main-bg'>
+      <header class='text-2xl font-semibold'>Gismeteo Clone</header>
+      <main class='container mx-auto py-4'>Content</main>
     </div>
   );
 };
