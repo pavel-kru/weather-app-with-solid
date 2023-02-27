c05d36c32d2cff6f4e115b92a2691f4fb8f408a5
Pavel Kruglik <pavel.k@symfonyart.com>
2023-02-27 11:48:34 +0300

    refactor: types

--- src/utils/click-outside.ts
+++ src/utils/click-outside.ts
@@ -2,17 +2,18 @@ import type { Accessor, JSX } from 'solid-js';
 import { onCleanup } from 'solid-js';
 
 export default function clickOutside(el: HTMLElement, accessor: Accessor<any>) {
-  const onClick: JSX.EventHandler<HTMLBodyElement, MouseEvent> = (e) =>
+  const onClick: JSX.EventHandler<HTMLElement, MouseEvent> = e =>
     !el.contains(e.target) && accessor()?.();
+
   document.body.addEventListener(
     'click',
-    onClick as EventListenerOrEventListenerObject
+    onClick as EventListenerOrEventListenerObject,
   );
 
   onCleanup(() =>
     document.body.removeEventListener(
       'click',
-      onClick as EventListenerOrEventListenerObject
-    )
+      onClick as EventListenerOrEventListenerObject,
+    ),
   );
 }
