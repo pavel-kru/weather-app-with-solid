948c2aeb5359d59691f4938fa9edc18b5068c994
Pavel Kruglik <pavel.k@symfonyart.com>
2023-02-27 09:14:30 +0300

    refactor: added documentation link

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const Tomorrow = lazy(() => import('./views/Tomorrow'));
 import { BaseWeatherFilters, projectApi } from './api';
 import { WeatherSearch } from './components';
 
+//https://www.solidjs.com/docs/latest/api#use___
 declare module 'solid-js' {
   namespace JSX {
     interface Directives {
