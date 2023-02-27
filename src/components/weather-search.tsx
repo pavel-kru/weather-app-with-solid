d449ef0ab0ad13b9f15c4e2b6605eb525e55d756
Pavel Kruglik <pavel.k@symfonyart.com>
2023-02-27 09:44:42 +0300

    style: search styles

--- src/components/weather-search.tsx
+++ src/components/weather-search.tsx
@@ -23,7 +23,6 @@ export const WeatherSearch: Component<WeatherSearchProps> = ({
   locations,
   setLatLong,
 }) => {
-
   return (
     <div
       //@ts-ignore
@@ -37,7 +36,9 @@ export const WeatherSearch: Component<WeatherSearchProps> = ({
           setSearch(e.currentTarget.value);
         }}
         type="text"
-        class="bg-gray-50 outline-none border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-44"
+        class={`bg-gray-50 outline-none border border-gray-300 text-gray-900 text-sm rounded-lg hover:border-blue-500 focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
+          show() ? 'w-64' : 'w-44'
+        } transition-all duration-100`}
         placeholder="Location"
         value={search()}
         data-dropdown-toggle="dropdown"
@@ -45,18 +46,20 @@ export const WeatherSearch: Component<WeatherSearchProps> = ({
       <Show fallback={<div />} when={show()}>
         <div
           id="dropdown"
-          class="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 absolute"
+          class={`z-10 bg-white divide-y divide-gray-100 rounded-lg shadow h-56 overflow-y-scroll scrollbar-hide::-webkit-scrollbar scrollbar-hide ${
+            show() ? 'w-64' : 'w-44'
+          } dark:bg-gray-700 absolute transition-all duration-100`}
         >
           <ul
-            class="p-4 text-sm text-gray-700 dark:text-gray-200 flex flex-col gap-2"
+            class="p-2 text-sm text-gray-700 dark:text-gray-200 flex flex-col gap-2"
             aria-labelledby="dropdownInput"
           >
             <For each={locations()?.features} fallback={<div>No Options</div>}>
               {item => (
                 <li
-                  class="cursor-pointer"
+                  class="cursor-pointer hover:bg-slate-200 px-2 rounded-md"
                   onClick={() => {
-                    setSearch(item.properties.address_line1);
+                    setSearch(item.properties.formatted);
                     setShow(false);
                     setLatLong({
                       lat: item.properties.lat,
@@ -64,7 +67,7 @@ export const WeatherSearch: Component<WeatherSearchProps> = ({
                     });
                   }}
                 >
-                  {item.properties.address_line1}
+                  {item.properties.formatted}
                 </li>
               )}
             </For>
